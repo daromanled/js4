621fc7fbcfbc5326df364100cadd5249604cfe9a
daromanled <da.led@mail.ru>
2017-05-17 12:46:10 +1000

    Update and rename LICENSE.md to index.js

---
+++ index.js
@@ -0,0 +1,122 @@
+/* ДЗ 4 - работа с DOM */
+
+/**
+ * Функция должна создать элемент с тегом DIV, поместить в него текстовый узел и вернуть получившийся элемент
+ *
+ * @param {string} text - текст, который необходимо поместить в div
+ * @return {Element}
+ */
+function createDivWithText(text) {
+    let d = document.createElement('div');
+    d.innerText = text;
+    return d;
+}
+
+/**
+ * Функция должна создать элемент с тегом A, установить значение для атрибута href и вернуть получившийся элемент
+ *
+ * @param {string} hrefValue - значение для атрибута href
+ * @return {Element}
+ */
+function createAWithHref(hrefValue) {
+    let a = document.createElement('a');
+    a.setAttribute('href', hrefValue);
+    return a;
+}
+
+/**
+ * Функция должна вставлять элемент what в начало элемента where
+ *
+ * @param {Element} what - что вставлять
+ * @param {Element} where - куда вставлять
+ */
+function prepend(what, where) {
+    where.insertBefore(what, where.firstChild);
+}
+
+/**
+ * Функция должна перебрать все дочерние элементы элемента where
+ * и вернуть массив, состоящий из тех дочерних элементов
+ * следующим соседом которых является элемент с тегом P
+ * Рекурсия - по желанию
+ *
+ * @param {Element} where - где искать
+ * @return {Array<Element>}
+ *
+ * @example
+ * для html '<div></div><p></p><a></a><span></span><p></p>'
+ * функция должна вернуть: [div, span]
+ * т.к. следующим соседом этих элементов является элемент с тегом P
+ */
+function findAllPSiblings(where) {
+    let res = [], arr = where.children;
+    for (let i=0; i<arr.length; i++)
+        if (arr[i].nextSibling && arr[i].nextSibling.tagName==="P")
+            res.push(arr[i]);
+    return res;
+}
+/**
+ * Функция должна перебрать все дочерние узлы типа "элемент" внутри where
+ * и вернуть массив, состоящий из текстового содержимого перебираемых элементов
+ * Но похоже, что в код закралась ошибка, которую нужно найти и исправить
+ *
+ * @param {Element} where - где искать
+ * @return {Array<string>}
+ */
+function findError(where) {
+    let res = [];
+    for (let x of where.children)
+        res.push(x.innerText);
+    return res;
+}
+
+/**
+ * Функция должна перебрать все дочерние узлы элемента where
+ * и удалить из него все текстовые узлы
+ * Без рекурсии!
+ * Будьте внимательны при удалении узлов,
+ * можно получить неожиданное поведение при переборе узлов
+ *
+ * @param {Element} where - где искать
+ *
+ * @example
+ * после выполнения функции, дерево <div></div>привет<p></p>loftchool!!!
+ * должно быть преобразовано в <div></div><p></p>
+ */
+function deleteTextNodes(where) {
+    let arr = where.childNodes;
+    for (let x of arr)
+        if (x.nodeType === 3)
+            where.removeChild(x);
+}
+
+
+/**
+ * Выполнить предудыщее задание с использование рекурсии
+ * то есть необходимо заходить внутрь каждого дочернего элемента
+ *
+ * @param {Element} where - где искать
+ *
+ * @example
+ * после выполнения функции, дерево <span> <div> <b>привет</b> </div> <p>loftchool</p> !!!</span>
+ * должно быть преобразовано в <span><div><b></b></div><p></p></span>
+ */
+function deleteTextNodesRecursive(where) {
+    let arr = where.childNodes;
+    for (let i=0;i<arr.length;i++)
+        if (arr[i].nodeType===3){
+            where.removeChild(arr[i]);
+            i--;}
+        else
+            deleteTextNodesRecursive(arr[i]);
+}
+
+export {
+    createDivWithText,
+    createAWithHref,
+    prepend,
+    findAllPSiblings,
+    findError,
+    deleteTextNodes,
+    deleteTextNodesRecursive,
+};
